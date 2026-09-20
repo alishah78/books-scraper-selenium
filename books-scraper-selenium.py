13a22d960aa484a81cfb64fcd59d013288317047
AliShah-Python <144084723+alishah78@users.noreply.github.com>
2026-09-20 13:12:35 +0500

    Refactor scraping script and update print messages

--- books-scraper-selenium.py
+++ books-scraper-selenium.py
@@ -13,7 +13,8 @@ prices = []
 stocks = []
 
 books = driver.find_elements(By.CLASS_NAME, "product_pod")
-print(f"Total books mile: {len(books)}") 
+print(f"Total books found: {len(books)}")
+
 for book in books:
     name = book.find_element(By.TAG_NAME, "h3").find_element(By.TAG_NAME, "a").get_attribute("title")
     price_text = book.find_element(By.CLASS_NAME, "price_color").text.strip()
@@ -23,13 +24,16 @@ for book in books:
     titles.append(name)
     prices.append(price)
     stocks.append(stock)
+
 driver.quit()
 
-df =pd.DataFrame({"Title": titles, "Price": prices, "Stock": stocks})
+df = pd.DataFrame({"Title": titles, "Price": prices, "Stock": stocks})
 df.drop_duplicates(inplace=True)
+
 with pd.ExcelWriter("books_report.xlsx", engine="openpyxl") as writer:
     df.to_excel(writer, index=False, sheet_name="Books")
     worksheet = writer.sheets["Books"]
     for cell in worksheet[1]:
         cell.font = Font(bold=True)
-print("sheet ok")
+
+print("Excel file created successfully!")
